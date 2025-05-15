e84683c20720ec3e8f1cb77b5cfe602aac288f08
Manoj Shrestha <manojxtha1000@gmail.com>
2025-05-15 16:27:58 +0545

    fixed issue

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard.main');
+Route::middleware('auth')->group(function () {
+
+    Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'dashboard'])->name('dashboard');
+
+    Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [App\Http\Controllers\ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/test', function () {
-    return view('dashboard.test');
-});
+require __DIR__ . '/auth.php';
